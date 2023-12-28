1f804a104dc763679e029abd6a624ac5b96d90ce
miri <mk6764576@gmail.com>
2023-12-28 04:48:23 +0200

    secondTry

---
+++ Bank.Data/Migrations/20231228021422_BankDb.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bank.Data.Migrations
+{
+    public partial class BankDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bank_AccountList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BankNumber = table.Column<int>(type: "int", nullable: false),
+                    BranchNumber = table.Column<int>(type: "int", nullable: false),
+                    OwnersName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OwnersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bank_AccountList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomerList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerList", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OfficialList",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Addreess = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    BranchNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OfficialList", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bank_AccountList");
+
+            migrationBuilder.DropTable(
+                name: "CustomerList");
+
+            migrationBuilder.DropTable(
+                name: "OfficialList");
+        }
+    }
+}
---
+++ Bank.core/Entities/Bank Account.cs
@@ -0,0 +1,22 @@
+﻿namespace Bank.Entities
+{
+    public class Bank_Account
+    {
+        public int BankNumber { get; set; }
+        public int BranchNumber { get; set; }
+        public int Id { get; set; }
+        public string OwnersName { get; set; }
+        public int OwnersId { get; set; }
+
+        public Bank_Account(int bankNumber, int branchNumber, int id, string ownersName, int ownersId)
+        {
+            BankNumber = bankNumber;
+            BranchNumber = branchNumber;
+            Id = id;
+            OwnersName = ownersName;
+            OwnersId = ownersId;
+        }
+
+       
+    }
+}
